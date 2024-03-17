a2c38ec37481c51a2d8842b4bd42b0b5991149c7
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-03-17 21:43:36 +0530

    Fixed Responsive

--- src/components/Products/ProductsPage.jsx
+++ src/components/Products/ProductsPage.jsx
@@ -4,7 +4,8 @@ import ItemsPage from "../ItemPage";
 
 const ProductsPage = ({ items, category }) => {
   const navigate = useNavigate();
-  const firstFiveItems = items.slice(0, 5);
+  const firstItemsCount = window.innerWidth < 768 ? 2 : 5; // Change the count based on screen width
+  const firstItems = items.slice(0, firstItemsCount);
 
   const redirectPageHandler = () => {
     // Redirect to the individual product page for the specific category
@@ -12,18 +13,20 @@ const ProductsPage = ({ items, category }) => {
   };
 
   return (
-    <div className="flex flex-row overflow-scroll">
-      {firstFiveItems.map((item) => (
+    <div className="flex flex-row">
+      {firstItems.map((item) => (
         <div key={item.id} className=" ">
           <ItemsPage data={item} />
         </div>
-      ))}6
+      ))}
+      {/* {window.innerWidth >= 768 && ( */}
       <div
         onClick={redirectPageHandler}
         className="text-red-700 border-2 border-black flex font-bold cursor-pointer items-center justify-center"
       >
         See More
       </div>
+      {/* )} */}
     </div>
   );
 };
